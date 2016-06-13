8ec83c4856c7aaa9533aa06b3ebbe5449c453f5d
wPef <f.demoncade@gmail.com>
2016-06-13 16:16:11 +0200

    added ERROR macro + handle termerror

--- src/error.c
+++ src/error.c
@@ -2,19 +2,36 @@
 
 int	sl_error(char *id, t_infos *infos)
 {
-	if (ft_strcmp("winsize", id) == 0)
+	if (ERROR("winsize"))
 	{
 		get_winsize(infos);
 		return (run_select(infos));
 	}
-	else if (ft_strcmp("usage", id) == 0)
+	else if (ERROR("usage"))
 	{
-		ft_putendl("usage :\tft_select\n\t./ft_select [file1, file2, file3, ...]");
+		ft_putendl("usage :\tft_select");
+		ft_putendl("\t./ft_select [file1, file2, file3, ...]");
 		return (-1);
 	}
 	else
+		return (sl_termerror(id, NULL));
+}
+
+int	sl_termerror(char *id, char *termtype)
+{
+	ft_putendl("ERROR :\tft_select");
+	if (ERROR("not set"))
 	{
-		ft_sdebug("UNEXEPCTED ERROR : %", id);
-		return (-1);
+		ft_putstr("\tSpecify a terminal type ");
+		ft_putendl("with 'export TERM <yourtype>'.");
 	}
+	else if (ERROR("terminfo"))
+		ft_putendl("\t Could not access the terminfo data base.");
+	else if (ERROR("type"))
+		ft_sdebug("\tTerminal type % is not define.", termtype);
+	else if (ERROR("info"))
+		ft_putendl("\tTerm info not reached");
+	else	
+		ft_sdebug("UNEXEPCTED ERROR :\tft_select\n\t%", id);
+	return (-1);
 }
